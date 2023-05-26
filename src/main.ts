4264e1a17de9b2c4c1bc190c8bb7e2a5a99842c1
SujhavMaskey <134535119+SujhavMaskey@users.noreply.github.com>
2023-05-26 07:03:17 +0545

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// import express from 'express'
+
+// const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.listen(3000)
+
+// const add =(a: number, b: number):number => a + b;
+
+// console.log(add(2,2))
+
+console.log('hello');
+
+console.log('hi');
